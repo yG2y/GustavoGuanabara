43c7410611a0d9358804e4ddbffbdeb3812d8c86
yG2y <g2002souzajardim@gmail.com>
2023-05-07 16:37:04 -0300

    fim da aula 12

---
+++ CursoJS/aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65){
+     (idade < 18) 
+        console.log('Voto opcional')
+}   else {
+    console.log('Voto obrigatório')
+}
